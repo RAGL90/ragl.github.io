b8f1772911b247cb4d457f01401273f46a7f57c8
RAGL90 <ricgarlue@gmail.com>
2023-12-11 13:05:16 +0100

    Task list terminado, se añaden nuevas funciones

--- js/app.js
+++ js/app.js
@@ -6,8 +6,11 @@ window.addEventListener("load", () => {
   let closeButton = alert.firstElementChild; //Siguiente elemento a alert es Close
   let input = document.querySelector("#task");
   let arrow = document.querySelector(".arrow");
-
-  
+  let done = document.querySelectorAll(".fa-circle-check");
+  let remove = document.querySelectorAll(".fa-trash");
+  let edit = document.querySelectorAll(".fa-pencil");
+  let task = document.querySelectorAll(".task");
+  //console.log(done); //Es una "NodeList"
   //console.log(arrow);
 
   closeButton.addEventListener("click", () => {
@@ -17,52 +20,149 @@ window.addEventListener("load", () => {
   //Conocer cuando el usuario se situa en el input
   input.addEventListener("focus", () => {
     document.addEventListener("keydown", (event) => {
-        //console.log(event.code);
-        //Desactivamos el Intro numerico
-        if (event.code=="NumpadEnter" || event.code=="Enter") {
-            event.preventDefault();
-        }
+      //console.log(event.code);
+      //Desactivamos el Intro numerico
+      if (event.code == "NumpadEnter" || event.code == "Enter") {
+        event.preventDefault();
+      }
       //Conocer cuando se pulsa una tecla
     });
   });
   //Ventana de notificación
-  arrow.addEventListener('click',(e) => {
+  arrow.addEventListener("click", (e) => {
     if (input.value.trim() == "") {
       //El atributo .value es el que contiene el contenido de cualquier input
       //Con la funcion .trim() evitamos los espacios que introduzca el usuario al principio y final del string
       console.log("Empty");
       e.preventDefault();
-      input.value = ''; // Resetemamos el valor si está vacio
+      input.value = ""; // Resetemamos el valor si está vacio
       alert.classList.remove("dismissible");
     } else {
-      text = input.value;//  Esta variable está creada arriba
-      input.value = ''; //Reseteamos para cuando le da el botón
+      text = input.value; //  Esta variable está creada arriba
+      input.value = ""; //Reseteamos para cuando le da el botón
       // console.log(text);
-      id = parseInt(document.querySelector("tbody")?.lastElementChild?.id)+ 1 || 0;
+      id =
+        parseInt(document.querySelector("tbody")?.lastElementChild?.id) + 1 ||
+        0;
       /* !!!!!!
       El símbolo "?" es condicional, lo intenta si lo encuentra y si no arroja el error.
       "|| 0" , es por si no hubiese ID en las tablas, no existe, el id será 0.
       */
       //console.log(id);
 
-      //newRow tiene añadido el ID, el TR y el contenido. AHORA LO INSERTAMOS tras tbody:
-      document.querySelector('tbody').appendChild(generateRow(id,text));
-      
+      //newRow es una función al que enviamos los parámetros ID y <tr> y el contenido. AHORA LO INSERTAMOS tras tbody:
+      document.querySelector("tbody").appendChild(generateRow(id, text));
+
       //Eliminamos la etiqueta de alerta o instrucciones
       if (!alert.classList.contains("dismissible")) {
-        alert.classList.add ("dismissible"); 
+        alert.classList.add("dismissible");
       }
     }
+  });
+  //Listener para Check de Tareas
 
+  //Done como "queryselectorAll" es una colección de elementos no puede ser tratado como una función
+
+  // done.addEventListener('click',(e) => {
+  //   console.log("Working");
+  //   });
+
+  //Listener para borrar filas (PAPELERA)
+  done.forEach((item) => {
+    //Teniendo en cuenta que DONE son arrays y por tanto ha que hacer un ForEach para cada elemento
+    item.addEventListener("click", (e) => {
+      taskDone(e); //Llamada a taskDone
+    });
   });
 
+  remove.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      removeTask(e, false); //Llamada a removeTask
+    });
+  });
+
+  edit.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      editTask(e, false); //Llamada a editTask (90)
+    });
+  });
+
+  //Bucle para Task
+  task.forEach((item) => {
+    item.addEventListener("focus", (e) => {
+      editTask(e, true); //Llamada a editTask (90)
+    });
+  });
 });
 
+// Functions (Funciones que no deben cargarse hasta ser llamadas)
 
-const generateRow = (id, text) =>{
-  let newRow = document.createElement('tr');
-      newRow.setAttribute("id",id); //Añadimos a la etiqueta html id el valor de la variable id
-      newRow.innerHTML = `
+//Funcion EditTask para editar tareas del Checklist
+const editTask = (e, onFocus) => {
+  let editable = e;
+  if (onFocus) {
+    //console.log(e.target);
+    editable.target.classList.add("editable");
+    document.addEventListener("keydown", (e) => {
+      //console.log(e.code);
+      if (e.code == "Escape") {
+        editable.target.classList.remove("editable");
+        editable.target.blur(); //Evento para quitar el foco
+        if (editable.target.innerHTML == "" || editable.target.textContent.trim() == "") {
+          removeTask(editable, true);
+          console.log("Línea borrada");
+        }
+      }
+    });
+    editable.target.addEventListener("blur", () => {
+      editable.target.classList.remove("editable");
+      if (editable.target.innerHTML == "" || editable.target.textContent.trim() == "") {
+        removeTask(editable, true);
+        console.log("Línea borrada");
+      }
+    });
+  } else {
+    let editable =
+      e.target.parentNode.parentNode.previousElementSibling.lastElementChild;
+    editable.classList.add("editable");
+    editable.focus();
+  }
+  //console.log(task);
+};
+
+//Funcion removeTask para eliminar tareas del Checklist
+const removeTask = (e, editing) => {
+  if (editing) {
+    e.target.parentNode.parentNode.remove(); //Cuando se le llama desde "editTask"
+  } else {
+    e.target.parentNode.parentNode.parentNode.remove(); //Cuando es llamado desde el Icono de Borrar
+  }
+};
+
+//Funcion taskDone para tachar tareas realizadas pulsando el botón verde
+const taskDone = (e) => {
+  let task = e.target.nextElementSibling;
+  console.log(task);
+  text = task.innerHTML;
+
+  if (text.includes("<del>")) {
+    console.log("DEL tag detected!");
+    task.innerHTML = task.firstElementChild.textContent;
+    task.setAttribute("data-completed", "false");
+    // task.innerHTML = text.firstElementChild.textContent; NO puede ser porque estamos haciendo referencia al innerHTML
+    // task.innerHTML = `${text}`; Tampoco puede ser, porque estamos reescribiendo TODO el <SPAN>
+  } else {
+    console.log("Without DEL tag");
+    task.innerHTML = `<del>${text}</del`;
+    task.setAttribute("data-completed", "true");
+  }
+};
+
+//Funcion generateRow para crear nuevas tareas del Checklist
+const generateRow = (id, text) => {
+  let newRow = document.createElement("tr");
+  newRow.setAttribute("id", id); //Añadimos a la etiqueta html id el valor de la variable id
+  newRow.innerHTML = `
       <td>
                   <i class="fa-solid fa-circle-check fa-2x"></i>
                   <span class="task" contenteditable="true">${text}</span>
@@ -80,6 +180,21 @@ const generateRow = (id, text) =>{
                   </span>
       </td>
       `;
-      console.log(newRow);
-      return newRow;
+     newRow.firstElementChild.firstElementChild.addEventListener("click",(e) => {
+      taskDone(e);
+     });
+     newRow.firstElementChild.lastElementChild.addEventListener("click", (e) => {
+      //console.log("Aqui escucho!");
+      editTask(e, true);
+     });
+     newRow.firstElementChild.nextElementSibling.firstElementChild.addEventListener("click", (e) => {
+      //console.log("Aqui escucho desde el icono!");
+      editTask(e, false);
+     });
+     newRow.lastElementChild.firstElementChild.addEventListener("click", (e) => {
+      removeTask(e, false);
+     });
+  return newRow;
 };
+
+//Añadir Done, To Do
